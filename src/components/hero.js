95feb9eb806385b08ab87eff34f04c2e9e04520a
Sam Low-Chappell <sam@hackeryou.com>
2020-02-03 15:25:52 -0500

    updated typed.js on hero image

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,18 +1,62 @@
 import React from 'react';
 import styles from './hero.module.css';
 import video from '../images/header.mp4';
+import Typed from 'typed.js';
 
-const Hero = () => (
-    <div className={styles.header}>
-        <video autoPlay muted loop id="myVideo" className={styles.video}>
-            <source src={video} type="video/mp4"></source>
-        </video>
+class TypedReact extends React.Component {
+    componentDidMount() {
+        // If you want to pass more options as props, simply add
+        // your desired props to this destructuring assignment.
+        const { strings } = this.props;
+        // You can pass other options here, such as typing speed, back speed, etc.
+        const options = {
+            strings: strings,
+            typeSpeed: 70,
+            startDelay: 700,
+            backSpeed: 50,
+            backDelay: 1500,
+            loop: true,
+            loopCount: Infinity,
+        };
+        // this.el refers to the <span> in the render() method
+        this.typed = new Typed(this.el, options);
+    }
 
-        <div className={styles.content}>
-            <h1>Xavier Leong</h1>
-            <p>Video Editor</p>
+    componentWillUnmount() {
+        // Make sure to destroy Typed instance on unmounting
+        // to prevent memory leaks
+        this.typed.destroy();
+    }
+
+    render() {
+        return (
+            <p>
+                <span
+                    style={{ whiteSpace: 'pre' }}
+                    ref={el => {
+                        this.el = el;
+                    }}
+                />
+            </p>
+        );
+    }
+}
+
+const Hero = () => {
+    return (
+        <div className={styles.header}>
+            <video autoPlay muted loop id="myVideo" className={styles.video}>
+                <source src={video} type="video/mp4"></source>
+            </video>
+
+            <div className={styles.content}>
+                <h1>Xavier Leong</h1>
+                <TypedReact
+                    strings={['Video Editor', 'Journalist', 'Traveler']}
+                />
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Hero;
