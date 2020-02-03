eacacfe7edcc9535161686c14df61ad77e85ff75
Sam Low-Chappell <sam@hackeryou.com>
2020-02-03 14:22:38 -0500

    initial look at header of home page

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,29 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+import styles from './header.module.css';
+import video from '../images/header.mp4';
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+    <header className={styles.header}>
+        <video autoPlay muted loop id="myVideo" className={styles.video}>
+            <source src={video} type="video/mp4"></source>
+        </video>
+        <div className="wrapper">
+            <div className={styles.content}>
+                <h1>Xavier Leong</h1>
+                <p>Video Editor</p>
+            </div>
+        </div>
+    </header>
+);
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+    siteTitle: PropTypes.string,
+};
 
 Header.defaultProps = {
-  siteTitle: ``,
-}
+    siteTitle: ``,
+};
 
-export default Header
+export default Header;
