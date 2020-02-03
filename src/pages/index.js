1c13ba406ea6c5baf66da280114331fcb3728cd2
Sam Low-Chappell <sam@hackeryou.com>
2020-02-03 16:00:37 -0500

    added some index content and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Hero from '../components/hero';
+import styles from './index.module.css';
 
 const IndexPage = () => (
     <div>
@@ -12,6 +13,51 @@ const IndexPage = () => (
             <Layout>
                 <SEO title="Home" />
                 <p>Welcome to your new portfolio, Xavier.</p>
+                <div className={styles.service}>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <div className={styles.serviceDescription}>
+                        <h2>Service 1</h2>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Cupiditate esse laudantium rem asperiores ex
+                            eligendi, saepe exercitationem voluptate modi?
+                            Assumenda animi distinctio fugiat. Velit assumenda
+                            possimus fugiat similique asperiores sed.
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.service}>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <div className={styles.serviceDescription}>
+                        <h2>Service 2</h2>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Cupiditate esse laudantium rem asperiores ex
+                            eligendi, saepe exercitationem voluptate modi?
+                            Assumenda animi distinctio fugiat. Velit assumenda
+                            possimus fugiat similique asperiores sed.
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.service}>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <div className={styles.serviceDescription}>
+                        <h2>Service 3</h2>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing
+                            elit. Cupiditate esse laudantium rem asperiores ex
+                            eligendi, saepe exercitationem voluptate modi?
+                            Assumenda animi distinctio fugiat. Velit assumenda
+                            possimus fugiat similique asperiores sed.
+                        </p>
+                    </div>
+                </div>
             </Layout>
         </div>
     </div>
